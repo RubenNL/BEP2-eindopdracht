a0e0f3e29b4b0264961f0c1a863df36a7bb464c6
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-09-01 14:55:14 +0200

    deploy naar tomcat server

--- src/main/java/nl/hu/bep2/casino/CasinoApplication.java
+++ src/main/java/nl/hu/bep2/casino/CasinoApplication.java
@@ -2,12 +2,18 @@ package nl.hu.bep2.casino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CasinoApplication {
+public class CasinoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CasinoApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CasinoApplication.class);
     }
 
 }
