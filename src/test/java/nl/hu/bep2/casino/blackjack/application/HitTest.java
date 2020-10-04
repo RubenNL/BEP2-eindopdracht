d2c34c93e8d7c5bf4128ccb029f3cc5ab79ad520
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-10-04 13:32:15 +0200

    delete user before create, for local tests

--- src/test/java/nl/hu/bep2/casino/blackjack/application/HitTest.java
+++ src/test/java/nl/hu/bep2/casino/blackjack/application/HitTest.java
@@ -4,7 +4,9 @@ import nl.hu.bep2.casino.blackjack.application.strategies.HitStrategy;
 import nl.hu.bep2.casino.blackjack.data.SpringPlayerRepository;
 import nl.hu.bep2.casino.blackjack.domain.PlayTable;
 import nl.hu.bep2.casino.blackjack.domain.Player;
+import nl.hu.bep2.casino.chips.data.SpringChipsRepository;
 import nl.hu.bep2.casino.security.application.UserService;
+import nl.hu.bep2.casino.security.data.SpringUserRepository;
 import nl.hu.bep2.casino.security.data.User;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,18 +14,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.event.annotation.BeforeTestExecution;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
 public class HitTest {
 	@Autowired
 	private TableService tableService;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private SpringChipsRepository chipsRepository;
+	@Autowired
+	private SpringUserRepository userRepository;
 	@Autowired
 	private HandService handService;
 	@Autowired
@@ -32,6 +40,18 @@ public class HitTest {
 	private HitStrategy hitStrategy;
 	@Test
 	void hitTest() {
+		try {
+			User user=userService.loadUserByUsername("admin");
+			try {
+				chipsRepository.delete(chipsRepository.findByUser(user).get());
+			} catch(Exception e) {}
+			try {
+				playerRepository.delete(playerRepository.findByUser(user).get());
+			} catch(Exception e) {}
+			userRepository.delete(user);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		userService.register("admin","admin","ad","min");
 		User user=userService.loadUserByUsername("admin");
 		PlayTable table1=tableService.newTable(2,user);
